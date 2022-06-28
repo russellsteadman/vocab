2042d4517c07a8480b871fd50d793a743408e3ae
Russell Steadman <code@steadman.email>
2022-06-28 10:00:48 -0400

    Add wasm release

--- vocab.go
+++ vocab.go
@@ -1,3 +1,6 @@
+//go:build !js
+// +build !js
+
 package main
 
 import (
@@ -5,10 +8,9 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"regexp"
-	"sort"
 	"strconv"
-	"strings"
+
+	"github.com/russellsteadman/vocab/internal/vocab"
 )
 
 const version = "v0.2.0"
@@ -28,19 +30,10 @@ func printHelp() {
 	fmt.Println("  -g, --group\t\t\tGroup words into # word groups")
 }
 
-var wordRegex = regexp.MustCompile("(?i)[^-'0-9a-zÀ-ÿ`]")
-var sepRegex = regexp.MustCompile("[\n\r\t—]")
-
-type Word struct {
-	name  string
-	count int
-}
-
 func main() {
 	fmt.Print("\033[H\033[2J")
 
 	args := os.Args
-	vars := make(map[string]string)
 
 	if len(args) < 2 {
 		fmt.Print("Missing any arguments\n\n")
@@ -48,9 +41,9 @@ func main() {
 		return
 	}
 
-	simple := false
-	group := 1
-	var err error
+	vocabInput := &vocab.VocabInput{}
+	inputFile := ""
+	outputFile := ""
 
 	for i, arg := range args {
 		if arg == "-h" || arg == "--help" {
@@ -61,142 +54,96 @@ func main() {
 			return
 		} else if arg == "-o" || arg == "--output" {
 			if i+1 < len(args) {
-				vars["output"] = args[i+1]
+				outputFile = args[i+1]
 			}
 		} else if arg == "-i" || arg == "--input" {
 			if i+1 < len(args) {
-				vars["input"] = args[i+1]
+				inputFile = args[i+1]
 			}
 		} else if arg == "-t" || arg == "--thresh" {
 			if i+1 < len(args) {
-				vars["threshold"] = args[i+1]
+				thresh, err := strconv.Atoi(args[i+1])
+				if err != nil {
+					fmt.Print("Missing threshold number (e.g. 2)\n\n")
+					printHelp()
+					return
+				}
+
+				vocabInput.Threshold = thresh
 			}
 		} else if arg == "-c" || arg == "--count" {
 			if i+1 < len(args) {
-				vars["count"] = args[i+1]
+				count, err := strconv.Atoi(args[i+1])
+				if err != nil {
+					fmt.Print("Missing word count number (e.g. 2)\n\n")
+					printHelp()
+					return
+				}
+
+				vocabInput.Count = count
 			}
 		} else if arg == "-s" || arg == "--simple" {
-			simple = true
+			vocabInput.Simple = true
 		} else if arg == "-g" || arg == "--group" {
 			if i+1 < len(args) {
-				group, err = strconv.Atoi(args[i+1])
+				group, err := strconv.Atoi(args[i+1])
 				if err != nil {
 					fmt.Print("Missing group number (e.g. 2)\n\n")
 					printHelp()
 					return
 				}
+
+				vocabInput.Group = group
 			}
 		}
 	}
 
-	if (vars["input"] == "") || (vars["output"] == "") {
+	if (inputFile == "") || (outputFile == "") {
 		fmt.Print("Missing input or output file\n\n")
 		printHelp()
 		return
-	} else if vars["threshold"] != "" && vars["count"] != "" {
-		fmt.Print("Cannot use both threshold and count\n\n")
-		printHelp()
-		return
-	} else if vars["input"] == vars["output"] {
+	} else if inputFile == outputFile {
 		fmt.Print("Input and output files cannot be the same\n\n")
 		printHelp()
 		return
 	}
 
-	file, err := os.Open(vars["input"])
+	file, err := os.Open(inputFile)
 	if err != nil {
-		fmt.Printf("Error opening input file: %s\n\n", vars["input"])
+		fmt.Printf("Error opening input file: %s\n\n", inputFile)
 		printHelp()
 		return
 	}
-	defer file.Close()
 
 	fileBytes, err := io.ReadAll(file)
+	file.Close()
 	if err != nil {
 		fmt.Print("Error reading input file, is it in use elsewhere?\n\n")
 		printHelp()
 		return
 	}
 
-	fileString := sepRegex.ReplaceAllString(string(fileBytes), " ")
-	fileWords := strings.Split(fileString, " ")
-	fileWordsFormatted := make([]string, 0, len(fileWords))
+	vocabInput.File = fileBytes
 
-	for _, word := range fileWords {
-		word = wordRegex.ReplaceAllString(word, "")
-		word = strings.ToLower(word)
-		if word != "" {
-			fileWordsFormatted = append(fileWordsFormatted, word)
-		}
-	}
-
-	if group > 1 {
-		for i := 0; i <= len(fileWordsFormatted)-group; i++ {
-			fileWordsFormatted[i] = strings.Join(fileWordsFormatted[i:i+group], " ")
-		}
-		fileWordsFormatted = fileWordsFormatted[:len(fileWordsFormatted)-group+1]
-	}
-
-	wordCount := make(map[string]int, len(fileWordsFormatted))
-
-	for _, word := range fileWordsFormatted {
-		if _, ok := wordCount[word]; !ok {
-			wordCount[word] = 1
-		} else {
-			wordCount[word]++
-		}
-	}
-
-	words := make([]Word, 0, len(wordCount))
-
-	for word, count := range wordCount {
-		words = append(words, Word{word, count})
-	}
-
-	sort.Slice(words, func(i int, j int) bool {
-		return words[i].count > words[j].count
-	})
-
-	file, err = os.Create(vars["output"])
+	vocabOutput, err := vocab.CreateSet(vocabInput)
 	if err != nil {
-		fmt.Printf("Error opening output file: %s\n\n", vars["output"])
+		fmt.Print(err.Error() + "\n\n")
 		printHelp()
 		return
 	}
-	defer file.Close()
 
-	max := len(words)
-	if vars["count"] != "" {
-		max, err = strconv.Atoi(vars["count"])
-		if err != nil {
-			fmt.Printf("Error parsing count: %s\n\n", vars["count"])
-			printHelp()
-			return
-		}
+	file, err = os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error opening output file: %s\n\n", outputFile)
+		printHelp()
+		return
 	}
 
-	thresh := 0
-	if vars["threshold"] != "" {
-		thresh, err = strconv.Atoi(vars["threshold"])
-		if err != nil {
-			fmt.Printf("Error parsing threshold: %s\n\n", vars["threshold"])
-			printHelp()
-			return
-		}
-	}
-
-	for i := 0; i < max; i++ {
-		if words[i].count >= thresh {
-			if simple {
-				file.WriteString(words[i].name + "\n")
-			} else {
-				file.WriteString(fmt.Sprintf("#%-5d - %5d\t%s\n", i+1, words[i].count, words[i].name))
-			}
-		}
-	}
+	file.Write(vocabOutput)
+	file.Close()
 
 	fmt.Println("Success!")
 	fmt.Print("Open the file in your editor to see the results:\n\n")
-	abs, _ := filepath.Abs(vars["output"])
+	abs, _ := filepath.Abs(outputFile)
 	fmt.Print(abs + "\n\n")
 }
